59b5fe090aba0eee7ab79cc60312e8f8304c49b5
Rod McNew <rmcnew@relivinc.com>
2021-10-03 23:19:42 -0500

    Update 2021-10-03_easy_two-sum.go

--- 2021-10-03_easy_two-sum.go
+++ 2021-10-03_easy_two-sum.go
@@ -8,7 +8,7 @@ import (
 func twoSum(nums []int, target int) []int {
     
     // Get the nums into a data structure that we can sort while remembering their indicies
-    var sortedNums = make([]struct{index, value int}, len(nums))
+    sortedNums := make([]struct{index, value int}, len(nums))
     for index, value := range nums {
         sortedNums[index].index = index
         sortedNums[index].value = value
