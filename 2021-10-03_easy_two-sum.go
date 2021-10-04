9b1dbb1b4188303c47e033ca3676e74e145945cb
Rod McNew <rmcnew@relivinc.com>
2021-10-03 22:59:00 -0500

    Update 2021-10-03_easy_two-sum.go

--- 2021-10-03_easy_two-sum.go
+++ 2021-10-03_easy_two-sum.go
@@ -5,29 +5,23 @@ import (
     "sort"
 )
 
-	
-type num struct {
-    index int
-    value int
-}
-
 func twoSum(nums []int, target int) []int {
     
-    // Get the nums sorted while remembering their original indices
-    var sortedNums = make([]num, len(nums))
+    // Get the nums into a data structure that we can sort
+    var sortedNums = make([]struct{index, value int}, len(nums))
     for index, value := range nums {
         sortedNums[index].index = index
         sortedNums[index].value = value
     }
-    sort.Slice(sortedNums, func(a int, b int) bool {return sortedNums[a].value < sortedNums[b].value})
-    // fmt.Print("sorted Nums", sortedNums)
+    
+    // Sort the nums
+    sort.Slice(sortedNums, func(a, b int) bool {return sortedNums[a].value < sortedNums[b].value})
     
     // Move the left and right pointers inward until we find the match
     left := 0
     right := len(nums) - 1
     for left < right {
         var sum = sortedNums[left].value + sortedNums[right].value
-        // fmt.Print("in loop [", left, sortedNums[left].value, "] + [", right, sortedNums[right].value, "] = ", sum, "\n");
         if sum > target {
             right--;
         } else if sum < target {
